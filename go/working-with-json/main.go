9eb3aa05a3af92b30f98175d3c72364cb11e31e2
lowranceworks <lowranceworks@gmail.com>
2023-03-06 21:02:50 -0600

    go: working with json, convert json with nested element to struct using json.unmarshal.

--- go/working-with-json/main.go
+++ go/working-with-json/main.go
@@ -6,14 +6,26 @@ import (
 )
 
 type SensorReading struct {
-	Name     string `json:"name"`
-	Capacity int    `json:"capacity"`
-	Time     string `json:"time"`
+	Name        string `json:"name"`
+	Capacity    int    `json:"capacity"`
+	Time        string `json:"time"`
+	Information Info   `json:"info"`
+}
+
+type Info struct {
+	Description string `json:"desc"`
 }
 
 func main() {
 
-	jsonString := `{"name": "battery sensor", "capacity": 40, "time": "2023-03-21T19:08:27Z"}`
+	jsonString := `{
+		"name": "battery sensor",
+		"capacity": 40,
+		"time": "2023-03-21T19:08:27Z",
+		"info": {
+			"desc": "a sensor reading"
+	}}`
+
 	var reading SensorReading
 	err := json.Unmarshal([]byte(jsonString), &reading)
 	if err != nil {
