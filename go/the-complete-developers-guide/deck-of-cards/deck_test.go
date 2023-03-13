6c89bc0c924b41088034b13f5e2e0d151582dce7
lowranceworks <lowranceworks@gmail.com>
2023-03-13 10:10:59 -0500

    docs: update test notes.

--- go/the-complete-developers-guide/deck-of-cards/deck_test.go
+++ go/the-complete-developers-guide/deck-of-cards/deck_test.go
@@ -2,6 +2,10 @@ package main
 
 import "testing"
 
+// how do we know what to test?
+// well as the developer, what do you care about?
+// when a deck is created, it always has 16 cards in it.
+// one test we can have is checking the length of the deck and making sure it is 16.
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
@@ -9,3 +13,5 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
+
+// run `go test` in your terminal to run all files ending in `_test.go`.
