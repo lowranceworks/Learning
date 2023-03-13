3750c55b744b0b61c38ca797d38a6b7e514a94e7
lowranceworks <lowranceworks@gmail.com>
2023-03-13 10:56:53 -0500

    test: create new test: TestSaveToDeckAndNewDeckFromFile

--- go/the-complete-developers-guide/deck-of-cards/deck_test.go
+++ go/the-complete-developers-guide/deck-of-cards/deck_test.go
@@ -1,12 +1,18 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+// a test is really nothing more than a go function that is executed when you run `go test` in your terminal.
+// run `go test` in your terminal to run all files ending in `_test.go`.
 
 // how do we know what to test?
 // well as the developer, what do you care about?
 // when a deck is created, it always has 16 cards in it.
 // one test we can have is checking the length of the deck and making sure it is 16.
-func TestNewDeck(t *testing.T) { // tests will always have
+func TestNewDeck(t *testing.T) { // tests will always take an argument of (t *testing.T)
 	d := newDeck()
 
 	// test 1
@@ -25,4 +31,18 @@ func TestNewDeck(t *testing.T) { // tests will always have
 	}
 }
 
-// run `go test` in your terminal to run all files ending in `_test.go`.
+// the reason for the long test name:
+// if you make a change to the newDeckFromFile() function inside deck.go,
+// you can copy/paste the name and find it in `deck_test.go`.
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting") // delete any files in the current working directory with the name "_decktesting"
+
+	deck := newDeck()               // create a deck
+	deck.saveToFile("_decktesting") // save to file "_decktesting"
+
+	loadedDeck := newDeckFromFile("_decktesting") // load from file
+
+	if len(loadedDeck) != 16 { // assert deck length
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+}
