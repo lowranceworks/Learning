1bf62b7f59c8d5100a810f6dcff62535fdf3466f
lowranceworks <lowranceworks@gmail.com>
2023-03-14 09:59:22 -0500

    feat: add example of embedding a struct inside of a struct.

--- go/the-complete-developers-guide/structs/main.go
+++ go/the-complete-developers-guide/structs/main.go
@@ -5,44 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	// basic usage of a struct
-	alex := person{"Alex", "Anderson"}
-	// if the order of the properties are swapped,  Alex's firstName will be set to his lastName.
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Jones",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 78750, // multiline structs must have a comma at the end of the property, even if it is the last property.
+		},
+	}
 
-	// the verbose approach to using a struct:
-	bob := person{firstName: "Bob", lastName: "Bones"}
-	charlie := person{lastName: "Cobb", firstName: "Charlie"}
-	// when you use this approach you can change the order of the properties without consequence.
-
-	fmt.Println(alex)
-	fmt.Println(bob)
-	fmt.Println(charlie)
-
-	// there is one more way to declare a struct.
-	var josh person
-	fmt.Println(josh)
-	// returns zero values:
-	// {  }
-
-	fmt.Printf("%+v\n", josh)
-	// returns properties and zero values:
-	// {firstName: lastName:}
-
-	// each empty attribute defaults to a zero value if you do not use it.
-
-	// zero values for respective types:
-	// string -> ""
-	// int -> 0
-	// float -> 0
-	// bool -> false
-
-	// you may want to set the default values to something else such as nil.
-
-	// you can update the properties of an object using a struct:
-	josh.firstName = "Josh"
-	josh.lastName = "Lowrance"
-	fmt.Println(josh)
+	fmt.Printf("%+v", jim) // {firstName:Jim lastName:Jones contact:{email:jim@gmail.com zipCode:78750}}
 }
